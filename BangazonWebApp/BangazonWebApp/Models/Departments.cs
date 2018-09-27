cb667a1e7589fe6c3cee5393bfd040d90d471167
Michael Roberts <35013526+MDR88@users.noreply.github.com>
2018-09-27 14:48:19 -0500

    Created the Departments Model.

---
+++ BangazonWebApp/BangazonWebApp/Models/Departments.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BangazonWebApp.Controllers
+{
+    public class Departments
+    {
+
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Budget { get; set; }
+
+
+    }
+
+
+}
+
