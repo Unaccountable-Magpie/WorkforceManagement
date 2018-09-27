4cbb76eb3e6b90ef69f71ef86b8a482188fc2208
unknown <brettcshearin@hotmail.com>
2018-09-27 14:20:30 -0500

    Working on Departments Create

---
+++ BangazonWebApp/BangazonWebApp/Controllers/DepartmentsController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Workforce.Models;
+using Workforce.Models.ViewModels;
+using System.Data.SqlClient;
+
+namespace Workforce.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public DepartmentController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            string sql = $@"
+            select
+                s.Id,
+                s.Name,
+                s.Budget,                
+            from Department s
+            WHERE s.Id = {id}";
+
+            using (IDbConnection conn = Connection)
+            {
+
+                Department department = (await conn.QueryAsync<Department>(sql)).ToList().Single();
+
+                if (department == null)
+                {
+                    return NotFound();
+                }
+
+                return View(department);
+            }
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Department department)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string sql = $@"
+                    INSERT INTO Department
+                        ( Name, Budget)
+                        VALUES
+                        ( 
+                             '{department.Name}'
+                            , '{department.Budget}'
+                            
+                        )
+                    ";
+
+                using (IDbConnection conn = Connection)
+                {
+                    int rowsAffected = await conn.ExecuteAsync(sql);
+
+                    if (rowsAffected > 0)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+
+
+        }
+
+
+    }
+}
