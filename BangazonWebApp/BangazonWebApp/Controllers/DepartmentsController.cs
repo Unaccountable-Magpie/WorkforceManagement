c0a67fd37bfa72441adfe88bbe2631f74a6d061c
unknown <austin.gorman@gmail.com>
2018-09-27 14:01:14 -0500

    added computers model

---
+++ BangazonWebApp/BangazonWebApp/Controllers/DepartmentsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BangazonWebApp.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BangazonWebApp/BangazonWebApp/Models/Computers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWebApp.Models
+{
+    public class Computers
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Date Purchased")]
+        public DateTime DatePurchased { get; set; }
+        [Display(Name = "Decommissioned Date")]
+        [Required]
+        public DateTime DecommissionedDate { get; set; }
+        [Display(Name = "Needs Repair?")]
+        [Required]
+        public bool Malfunctioned { get; set; }
+    }
+}
